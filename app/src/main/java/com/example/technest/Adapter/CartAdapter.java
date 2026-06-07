4fd1f59c2d4f4b0331931b84f1506452f297a1ad
Natasya <natasyacaca916@gmail.com>
2026-06-07 22:21:42 +0800

    feat

---
+++ app/src/main/java/com/example/technest/Adapter/CartAdapter.java
@@ -0,0 +1,81 @@
+package com.example.technest.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.example.technest.Model.Product;
+import com.example.technest.R;
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
+
+    private List<Product> cartList;
+    private OnDeleteClickListener deleteListener;
+
+    public interface OnDeleteClickListener {
+        void onDeleteClick(Product product, int position);
+    }
+
+    public CartAdapter(List<Product> cartList, OnDeleteClickListener deleteListener) {
+        this.cartList = cartList;
+        this.deleteListener = deleteListener;
+    }
+
+    @NonNull
+    @Override
+    public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_cart, parent, false);
+        return new CartViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
+        Product product = cartList.get(position);
+
+        holder.tvTitle.setText(product.getTitle());
+        holder.tvPrice.setText("$" + product.getPrice());
+
+        Glide.with(holder.itemView.getContext())
+                .load(product.getThumbnail())
+                .placeholder(R.drawable.ic_technest_logo)
+                .centerCrop()
+                .into(holder.imgProduct);
+
+        holder.btnDelete.setOnClickListener(v -> {
+            if (deleteListener != null) {
+                deleteListener.onDeleteClick(product, position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return cartList != null ? cartList.size() : 0;
+    }
+
+    public void updateData(List<Product> newList) {
+        this.cartList = newList;
+        notifyDataSetChanged();
+    }
+
+    public static class CartViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgProduct;
+        TextView tvTitle, tvPrice;
+        ImageButton btnDelete;
+
+        public CartViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgProduct = itemView.findViewById(R.id.img_cart_product);
+            tvTitle = itemView.findViewById(R.id.tv_cart_title);
+            tvPrice = itemView.findViewById(R.id.tv_cart_price);
+            btnDelete = itemView.findViewById(R.id.btn_delete_cart);
+        }
+    }
+}
