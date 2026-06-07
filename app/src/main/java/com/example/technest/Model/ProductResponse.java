66580b807dc01513fbee9edda635d4b9e2136831
Natasya <natasyacaca916@gmail.com>
2026-06-07 22:22:30 +0800

    feat

--- app/src/main/java/com/example/technest/Model/ProductResponse.java
+++ app/src/main/java/com/example/technest/Model/ProductResponse.java
@@ -13,4 +13,7 @@ public class ProductResponse {
 
     public List<Product> getProducts() { return products; }
     public int getTotal() { return total; }
+
+    public void setProducts(List<Product> products) { this.products = products; }
+    public void setTotal(int total) { this.total = total; }
 }
