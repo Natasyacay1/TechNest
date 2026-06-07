99918c3d0f51678afdb20940c207ed629c96b9e0
Natasya <natasyacaca916@gmail.com>
2026-06-07 22:21:31 +0800

    feat

--- app/src/main/java/com/example/technest/API/ApiService.java
+++ app/src/main/java/com/example/technest/API/ApiService.java
@@ -4,13 +4,25 @@ import com.example.technest.Model.ProductResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
     @GET("products/category/smartphones?limit=20")
-    Call<ProductResponse> getAllProducts();
+    Call<ProductResponse> getSmartphones();
 
-    // Dibuat satu parameter saja agar pas dengan baris ProductRepository kamu
+    @GET("products/category/laptops?limit=20")
+    Call<ProductResponse> getLaptops();
+
+    @GET("products/category/tablets?limit=20")
+    Call<ProductResponse> getTablets();
+
+    @GET("products/category/mobile-accessories?limit=20")
+    Call<ProductResponse> getAksesoris();
+
     @GET("products/category/{category}?limit=20")
     Call<ProductResponse> getProductsByCategory(@Path("category") String category);
+
+    @GET("products/search")
+    Call<ProductResponse> searchProducts(@Query("q") String query);
 }
